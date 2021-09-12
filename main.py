cf1bb60a926731f84c8cf3644a186258ecfcb227
DomTricks <79032052+DomTech-exe@users.noreply.github.com>
2021-09-12 13:48:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 from discord import guild
 from keep_alive import keep_alive
+import random
 
 intents = discord.Intents.default()
 intents.members=True
@@ -12,7 +13,7 @@ bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-  await bot.change_presence(status=discord.Status.online, activity=discord.Game('.help'))
+  await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f".help in {len(bot.guilds)} servers!"))
   print ("Cray is ready!")
 
 
@@ -23,7 +24,7 @@ async def hello(ctx, *, args = None):
 
 @bot.command()
 async def ping(ctx, *, args = None):
-    await ctx.send('Pong!')
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
 
 @bot.command()
@@ -63,12 +64,22 @@ async def help(ctx):
   
   banCommand = "This commands bans a member from the Discord Server"
 
-  sayCommand = "This command makes Cray say whatever you want. Example: .say Hey there!"
+  sayCommand = "This command makes Cray say whatever you want. Example: .say Hey! Note: to use multiple words use quotation marks to each side of your sentense"
 
   muteCommand = "This command mutes a member in a Discord Server"
 
   unmuteCommand = "This command unmutes a member in a Discord Server"
 
+  clearCommand = "This command is able to clear messages and the number is the amount of messages that you want to clear! Example usage: .clear 5"
+
+  lockCommand = "This command locks a channel in a Discord Server. Usage: .lock #thechannelofyourchoice"
+
+
+  unlockCommand = "This command unlocks a channel in a Discord Server. Usage: .unlock #thechannelofyourchoice"
+
+  eightballCommand = "This command works like a magic 8ball! Type '.8ball (your question)' and the bot will respond to you with an answer! WOW!"
+
+  nickCommand = 'The nick command changes other members nicknames. Note: It works for moderators and higher only to run the command on other members. Example usage: .nick @someone "Example"'
 
   embed = discord.Embed(
     title=name 
@@ -90,7 +101,19 @@ async def help(ctx):
   embed.add_field(name=".mute", value=muteCommand, inline=False)
 
   embed.add_field(name=".unmute", value=unmuteCommand, inline=False)
+
+  embed.add_field(name=".clear", value=clearCommand, inline=False)
+
+  embed.add_field(name=".lock", value=lockCommand, inline=False)
+
+  embed.add_field(name=".unlock", value=unlockCommand, inline=False)
  
+  embed.add_field(name=".8ball", value=eightballCommand, inline=False)
+
+  embed.add_field(name=".nick", value=nickCommand, inline=False)
+ 
+  embed.set_footer(text='Cray Version 0.3')
+
   await ctx.send(embed=embed)
 
 @bot.event 
@@ -158,7 +181,50 @@ async def unmute(ctx, member: discord.Member):
   embed = discord.Embed(title="unmute", description=f" Unmuted-{member.mention}",colour=discord.Colour.light_gray())
   await ctx.send(embed=embed)
 
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def lock(ctx, channel : discord.TextChannel=None):
+  channel = channel or ctx.channel
+  overwrite = channel.overwrites_for(ctx.guild.default_role)
+  overwrite.send_messages = False
+  await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+  await ctx.send(':lock: Channel locked.')
 
+
+
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def unlock(ctx, channel : discord.TextChannel=None):
+  channel = channel or ctx.channel
+  overwrite = channel.overwrites_for(ctx.guild.default_role)
+  overwrite.send_messages = True
+  await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+  await ctx.send(':unlock: Channel unlocked.')
+
+
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def clear(ctx, amount: int, target: discord.Member = None):
+    """Clears X Messages"""
+    if target is None:
+        check = None
+    else:
+        check = lambda m: m.author == target
+
+    deleted = await ctx.channel.purge(limit=amount, check=check)
+    await ctx.send(f':broom: Deleted **{len(deleted)} ** messages.', delete_after=15)
+
+@bot.command(name="8ball")
+async def _8ball(ctx):
+  await ctx.send(random.choice(["Yes.", "No.", "Maybe.", "Nice try.", "Did you brang me cake?", "I'm not sure about that one...", "Ask me a different question", "Nah.", "Noice", "Now that's what I call pwetty epic", "Yes!!", "No!!", "Absolutely!", "Absolutely Not!", ":)", "Sorry I was distracted but no."]))
+
+
+
+
+@bot.command(pass_context=True)
+async def nick(ctx, member: discord.Member, nick):
+    await member.edit(nick=nick)
+    await ctx.send(f"I have changed {member.mention}'s nickname!")
 
 keep_alive()
 bot.run(os.getenv('TOKEN'))
